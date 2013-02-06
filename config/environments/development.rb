0a43e43aea6507e28ce3a69b76e0c53dff624c7f
kapil <kapil@kapil-Inspiron-1420.(none)>
2013-02-06 22:44:16 +0530

    remove 304 error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,6 @@ Bbrtg::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
-
-  config.serve_static_assets = true
+  config.assets.debug = false
+  config.assets.logger = false
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,6 @@ Bbrtg::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
-   config.serve_static_assets = true
+  config.assets.debug = false
+  config.assets.logger = false
 end
